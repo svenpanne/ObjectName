ffa55506e99ed1ae7e108eb4b900a901f95e601c
Sven Panne <svenpanne@gmail.com>
2014-10-24 14:24:22 +0200

    Go back to IO, Applicative was too general.

--- src/Data/ObjectName.hs
+++ src/Data/ObjectName.hs
@@ -20,16 +20,14 @@ module Data.ObjectName (
    ObjectName(..), GeneratableObjectName(..)
 ) where
 
-import Control.Applicative ( Applicative )
-import Data.Foldable ( traverse_ )
-import Data.Traversable ( sequenceA )
+import Control.Monad ( replicateM )
 
 --------------------------------------------------------------------------------
 
 -- | An 'ObjectName' is an explicitly handled identifier for API objects, e.g. a
 -- texture object name in OpenGL or a buffer object name in OpenAL.
---
 #if __GLASGOW_HASKELL__ < 708
+--
 -- Minimal complete definition: 'isObjectName' plus one of 'deleteObjectName' or
 -- 'deleteObjectNames'.
 #endif
@@ -40,23 +38,23 @@ class ObjectName a where
 #endif
    -- | Test if the given object name is currently in use, i.e. test if it has
    -- been generated, but not been deleted so far.
-   isObjectName :: Applicative f => a -> f Bool
+   isObjectName :: a -> IO Bool
 
    -- | Make the given object name available again, declaring it as unused.
-   deleteObjectName :: Applicative f => a -> f ()
+   deleteObjectName :: a -> IO ()
    deleteObjectName = deleteObjectNames . (:[])
 
    -- | Bulk version of 'deleteObjectName'.
-   deleteObjectNames:: Applicative f => [a] -> f ()
-   deleteObjectNames = traverse_ deleteObjectName
+   deleteObjectNames:: [a] -> IO ()
+   deleteObjectNames = mapM_ deleteObjectName
 
 -- | A 'GeneratableObjectName' is an 'ObjectName' which can be generated without
 -- creating an associated object at the same time, e.g. an OpenGL buffer object
 -- name. Note that e.g. OpenGL program object names do not fall into this
 -- category, because you can only create such a name together with a program
 -- object itself.
---
 #if __GLASGOW_HASKELL__ < 708
+--
 -- Minimal complete definition: One of 'genObjectName' or 'genObjectNames'.
 #endif
 
@@ -66,9 +64,9 @@ class ObjectName a => GeneratableObjectName  a where
 #endif
    -- | Generate a new unused object name. By generating the name, it becomes
    -- used.
-   genObjectName :: Applicative f => f a
+   genObjectName :: IO a
    genObjectName = fmap head $ genObjectNames 1
 
    -- | Bulk version of 'genObjectName'.
-   genObjectNames :: Applicative f => Int -> f [a]
-   genObjectNames = sequenceA . flip replicate genObjectName
+   genObjectNames :: Int -> IO [a]
+   genObjectNames = flip replicateM genObjectName
